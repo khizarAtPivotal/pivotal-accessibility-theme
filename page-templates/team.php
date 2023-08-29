2108e57be3a13a5f692dd0abc95da9008d1e3f40
khizarAtPivotal <khizar@pivotalaccessibility.com>
2023-08-29 11:51:00 +0530

    update

---
+++ page-templates/team.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Team
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/
+ *
+ * @package Pivotal Accessibility
+ */
+
+get_header();
